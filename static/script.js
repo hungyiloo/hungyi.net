ef6a818893acdafb2fbb761a763138c0f19f268d
Hung-Yi Loo <hungyi.loo@gmail.com>
2021-10-19 20:21:50 +0800

    Implement theme inversion toggle + localStorage memory

---
+++ static/script.js
@@ -0,0 +1,11 @@
+// Remember theme inversion
+document.querySelector('#inverter').addEventListener(
+  'change',
+  e => localStorage.setItem('themeInverted', String(e.target.checked))
+)
+
+// Google analytics
+window.dataLayer = window.dataLayer || [];
+function gtag() { dataLayer.push(arguments); }
+gtag('js', new Date());
+gtag('config', 'UA-180663322-1');
