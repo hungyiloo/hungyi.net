c5fa4b5a3c5b3332681a0d43fb217cfac01062e9
Hung-Yi Loo <hungyi.loo@gmail.com>
2024-07-31 14:46:42 +0800

    feat(graphics): add logo and favicon

--- quartz/components/PageTitle.tsx
+++ quartz/components/PageTitle.tsx
@@ -1,4 +1,4 @@
-import { pathToRoot } from "../util/path"
+import { joinSegments, pathToRoot } from "../util/path"
 import { QuartzComponent, QuartzComponentConstructor, QuartzComponentProps } from "./types"
 import { classNames } from "../util/lang"
 import { i18n } from "../i18n"
@@ -8,6 +8,15 @@ const PageTitle: QuartzComponent = ({ fileData, cfg, displayClass }: QuartzCompo
   const baseDir = pathToRoot(fileData.slug!)
   return (
     <h1 class={classNames(displayClass, "page-title")}>
+      <div style={{ display: "inline-block", position: "relative", bottom: "0.1em" }}>
+        <div style={{ display: "flex", alignItems: "center", justifyContent: "center", height: "1em", width: "1em", marginRight: "0.35em", padding: "0.1em", background: "#11111b", borderRadius: "1em" }}>
+          <img
+            src={joinSegments(baseDir, "static/HY_Light_and_Color_2.svg")}
+            alt="Logo"
+            style={{ height: "0.5em", margin: 0, padding: 0, borderRadius: "0" }}
+          />
+        </div>
+      </div>
       <a href={baseDir}>{title}</a>
     </h1>
   )
@@ -16,6 +25,7 @@ const PageTitle: QuartzComponent = ({ fileData, cfg, displayClass }: QuartzCompo
 PageTitle.css = `
 .page-title {
   margin: 0;
+  font-size: 130%;
 }
 `
 
