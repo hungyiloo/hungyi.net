953e2ffc9c3e0f8375c6b9300f566b542393ebb9
Hung-Yi Loo <hungyi.loo@gmail.com>
2024-07-31 13:48:50 +0800

    feat(config): retheme to catppuccin colors

--- quartz.config.ts
+++ quartz.config.ts
@@ -28,26 +28,26 @@ const config: QuartzConfig = {
       },
       colors: {
         lightMode: {
-          light: "#faf8f8",
-          lightgray: "#e5e5e5",
-          gray: "#b8b8b8",
-          darkgray: "#4e4e4e",
-          dark: "#2b2b2b",
-          secondary: "#284b63",
-          tertiary: "#84a59d",
-          highlight: "rgba(143, 159, 169, 0.15)",
-          textHighlight: "#fff23688",
+          light: "#e6e9ef", // Mantle
+          lightgray: "#ccd0da", // Surface 0
+          gray: "#9ca0b0",
+          darkgray: "#5c5f77",
+          dark: "#4c4f69",
+          secondary: "#179299", // Teal
+          tertiary: "#7287fd", // Lavender
+          highlight: "#7c7f9311",
+          textHighlight: "#df8e1d88",
         },
         darkMode: {
-          light: "#161618",
-          lightgray: "#393639",
-          gray: "#646464",
-          darkgray: "#d4d4d4",
-          dark: "#ebebec",
-          secondary: "#7b97aa",
-          tertiary: "#84a59d",
-          highlight: "rgba(143, 159, 169, 0.15)",
-          textHighlight: "#b3aa0288",
+          light: "#11111b", // Crust
+          lightgray: "#45475a", // Surface 1
+          gray: "#6c7086", // Overlay 0
+          darkgray: "#bac2de", // Subtext 1
+          dark: "#cdd6f4", // Text
+          secondary: "#89b4fa", // Blue
+          tertiary: "#cba6f7", // Mauve
+          highlight: "#9399b218", // Overlay 2
+          textHighlight: "#f9e2af88", // Yellow
         },
       },
     },
@@ -60,8 +60,8 @@ const config: QuartzConfig = {
       }),
       Plugin.SyntaxHighlighting({
         theme: {
-          light: "github-light",
-          dark: "github-dark",
+          light: "catppuccin-latte",
+          dark: "catppuccin-mocha",
         },
         keepBackground: false,
       }),
