c5549691e60a23356650a188eb54b0ddd9fc8620
Hung-Yi Loo <hungyi.loo@gmail.com>
2024-08-02 11:38:26 +0800

    feat(styles): more typography tweaks

--- quartz.config.ts
+++ quartz.config.ts
@@ -22,8 +22,8 @@ const config: QuartzConfig = {
       fontOrigin: "googleFonts",
       cdnCaching: true,
       typography: {
-        header: "Inter",
-        body: "Source Sans 3",
+        header: "Fira Sans",
+        body: "Assistant",
         code: "JetBrains Mono",
       },
       colors: {
--- quartz/util/theme.ts
+++ quartz/util/theme.ts
@@ -34,7 +34,7 @@ const DEFAULT_MONO = "ui-monospace, SFMono-Regular, SF Mono, Menlo, monospace"
 
 export function googleFontHref(theme: Theme) {
   const { code, header, body } = theme.typography
-  return `https://fonts.googleapis.com/css2?family=${code}&family=${header}:wght@400;700&family=${body}:ital,wght@0,400;0,600;1,400;1,600&display=swap`
+  return `https://fonts.googleapis.com/css2?family=${code}:ital,wght@0,400..700;1,400..700&family=${header}:wght@300;400;500;600;700;800&family=${body}:ital,wght@0,400..700;1,400..700&display=swap`
 }
 
 export function joinStyles(theme: Theme, ...stylesheet: string[]) {
