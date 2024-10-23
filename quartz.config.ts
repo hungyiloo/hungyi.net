828a8817ffd6211397e17bfdd9edd04969b4498b
Hung-Yi Loo <hungyi.loo@gmail.com>
2024-10-23 16:35:37 +0800

    feat: use Figtree for header font instead of Fira Sans

--- quartz.config.ts
+++ quartz.config.ts
@@ -22,7 +22,7 @@ const config: QuartzConfig = {
       fontOrigin: "googleFonts",
       cdnCaching: true,
       typography: {
-        header: "Fira Sans",
+        header: "Figtree",
         body: "Assistant",
         code: "JetBrains Mono",
       },
